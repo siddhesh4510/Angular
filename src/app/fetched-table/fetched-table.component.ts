05fa0268a43be7b80e2ee530a3aa5385fad3d811
siddhesh4510 <65489408+siddhesh4510@users.noreply.github.com>
2021-09-16 17:33:03 +0530

    Update fetched-table.component.ts

--- src/app/fetched-table/fetched-table.component.ts
+++ src/app/fetched-table/fetched-table.component.ts
@@ -10,16 +10,22 @@ export class FetchedTableComponent implements OnInit {
 
   constructor(private getDataService: GetapidataService) { }
   public apidata:any=[];
+  public displaydata:any=[];
+  public paginationArray:any=[];
 
   ngOnInit(): void {
     this.getDataService.getApiData().subscribe(data => {
       this.apidata = data;
+      this.displaydata=this.apidata.slice(0,10);
+      this.paginationArray=Array.from({length: this.apidata.length/10}, (_, i) => i + 1)
+      console.log(this.paginationArray)
       
     }
-    
     )
-    
-  
+  }
+  onPagination(page:number){
+    this.displaydata=this.apidata.slice(10*(page-1), 10*(page-1)+10);
+
   }
   getData(){
     
